c911202aba3f7e27629d15439a3fd1232e22668c
frozenhelium <fren.ankit@gmail.com>
2021-03-25 15:37:46 +0545

    Implement InputHint component

--- src/components/InputContainer/index.tsx
+++ src/components/InputContainer/index.tsx
@@ -3,6 +3,7 @@ import { _cs } from '@togglecorp/fujs';
 
 import InputLabel from '#components/InputLabel';
 import InputError from '#components/InputError';
+import InputHint from '#components/InputHint';
 import InputBorder from '#components/InputBorder';
 import { UiMode } from '#components/UiModeContext';
 import useUiModeClassName from '#hooks/useUiModeClassName';
@@ -74,7 +75,7 @@ function InputContainer(props: Props) {
             )}
         >
             <InputLabel
-                className={_cs(styles.inputLable, labelContainerClassName)}
+                className={labelContainerClassName}
                 uiMode={uiMode}
             >
                 {label}
@@ -101,13 +102,13 @@ function InputContainer(props: Props) {
                     errored={!!error}
                 />
             </div>
-            <InputError className={_cs(styles.error, errorContainerClassName)}>
+            <InputError className={errorContainerClassName}>
                 {error}
             </InputError>
             {!error && hint && (
-                <div className={_cs(styles.hint, hintContainerClassName)}>
+                <InputHint className={hintContainerClassName}>
                     {hint}
-                </div>
+                </InputHint>
             )}
         </div>
     );
