5594f3e176617fb49ce2a3b2f076b361b5ad3b46
frozenhelium <fren.ankit@gmail.com>
2021-03-25 15:21:27 +0545

    Implement DateInput

---
+++ src/stories/DateInput.stories.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { withDesign } from 'storybook-addon-designs';
+import { Story } from '@storybook/react/types-6-0';
+import { useArgs } from '@storybook/client-api';
+import { IoText } from 'react-icons/io5';
+
+import DateInput, { Props as DateInputProps } from '#components/DateInput';
+
+export default {
+    title: 'Input/DateInput',
+    component: DateInput,
+    argTypes: {},
+    decorators: [withDesign],
+};
+
+const Template: Story<DateInputProps<string>> = (args) => {
+    const [{ value }, updateArgs] = useArgs();
+
+    const handleChange = (e: string | undefined) => {
+        updateArgs({ value: e });
+    };
+
+    return (
+        <DateInput
+            {...args}
+            value={value}
+            onChange={handleChange}
+        />
+    );
+};
+
+export const Default = Template.bind({});
+Default.args = {
+    label: 'Birthdate',
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+    label: 'Birthdate',
+    value: '1992-12-27',
+    disabled: true,
+};
+
+export const ReadOnly = Template.bind({});
+ReadOnly.args = {
+    label: 'Birthdate',
+    value: '1992-12-27',
+    readOnly: true,
+};
