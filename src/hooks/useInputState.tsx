4933304cd7832fd06595b8afbdff5e21a624ac0e
frozenhelium <fren.ankit@gmail.com>
2021-06-09 10:56:32 +0545

    Do a beta release with TextOutput component

--- src/hooks/useInputState.tsx
+++ src/hooks/useInputState.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 
+type SetterFn<T> = (oldValue: T) => T;
+
 function useInputState<T>(initialValue: T) {
     const [value, setValue] = React.useState<T>(initialValue);
 
     type SetValue = React.Dispatch<React.SetStateAction<T>>;
     const setValueSafe: SetValue = React.useCallback((newValueOrSetter) => {
         setValue((oldValue) => (
+            // NOTE: explicit typecast is required because of
+            // https://github.com/microsoft/TypeScript/issues/37663
             typeof newValueOrSetter === 'function'
-                ? newValueOrSetter(oldValue)
+                ? (newValueOrSetter as SetterFn<T>)(oldValue)
                 : newValueOrSetter
         ));
     }, []);
