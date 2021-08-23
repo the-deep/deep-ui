3ec40742a326296b417fbcdfcc999cf1a707bb90
tnagorra <weathermist@gmail.com>
2021-08-23 15:18:41 +0545

    Add ControlledExpandableContainer

--- src/components/ExpandableContainer/index.tsx
+++ src/components/ExpandableContainer/index.tsx
@@ -1,100 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { _cs } from '@togglecorp/fujs';
-import {
-    IoChevronUpOutline,
-    IoChevronDownOutline,
-} from 'react-icons/io5';
 
-import useBooleanState from '../../hooks/useBooleanState';
-import Container, { Props as ContainerProps } from '../Container';
-import Button from '../Button';
-
-import styles from './styles.css';
+import { Props as ContainerProps } from '../Container';
+import ControlledExpandableContainer from '../ControlledExpandableContainer';
 
 export interface Props extends ContainerProps {
-    defaultVisibility?: boolean;
     // NOTE: Mount will mount the child even if its not shown
     alwaysMountContent?: boolean;
-    expansionTriggerArea?: 'header' | 'arrow';
     disabled?: boolean;
     expansionButtonClassName?: string;
+    expansionTriggerArea?: 'header' | 'arrow';
+
+    defaultVisibility?: boolean;
 }
 
 function ExpandableContainer(props: Props) {
     const {
-        className,
-        heading,
-        children,
-        headingDescription,
         defaultVisibility = false,
-        alwaysMountContent = true,
-        headerActions,
-        headingContainerClassName,
-        headingClassName,
-        contentClassName,
-        headerClassName,
-        headingSize,
-        disabled = false,
-        expansionTriggerArea = 'header',
-        expansionButtonClassName,
         ...otherProps
     } = props;
 
-    const [
-        showContent,,,,
-        toggleContentVisibility,
-    ] = useBooleanState(defaultVisibility);
-
-    const mountContent = alwaysMountContent || showContent;
+    const [showContent, setContent] = useState(defaultVisibility ?? false);
 
     return (
-        <Container
+        <ControlledExpandableContainer
             {...otherProps}
-            className={_cs(
-                className,
-                styles.expandableContainer,
-            )}
-            headerElementProps={{
-                onClick: expansionTriggerArea === 'header' && !disabled
-                    ? toggleContentVisibility
-                    : undefined,
-            }}
-            headerClassName={_cs(
-                styles.header,
-                headerClassName,
-                expansionTriggerArea === 'header' && !disabled && styles.clickableHeader,
-            )}
-            headingContainerClassName={_cs(styles.headingContainer, headingContainerClassName)}
-            headingClassName={_cs(styles.heading, headingClassName)}
-            heading={heading}
-            headingSize={headingSize ?? 'small'}
-            headerActions={(
-                <>
-                    {headerActions}
-                    <Button
-                        className={_cs(styles.expandButton, expansionButtonClassName)}
-                        name={undefined}
-                        onClick={expansionTriggerArea === 'arrow' ? toggleContentVisibility : undefined}
-                        variant="action"
-                        disabled={disabled}
-                    >
-                        {showContent ? (
-                            <IoChevronUpOutline className={styles.icon} />
-                        ) : (
-                            <IoChevronDownOutline className={styles.icon} />
-                        )}
-                    </Button>
-                </>
-            )}
-            headingDescription={headingDescription}
-            contentClassName={_cs(
-                contentClassName,
-                styles.content,
-                showContent && styles.visible,
-            )}
-        >
-            {mountContent && children}
-        </Container>
+            name={undefined}
+            expanded={showContent}
+            onExpansionChange={setContent}
+        />
     );
 }
 
