0b125a2874bbe0279b7ec4debbdc555542f3a7a1
tnagorra <weathermist@gmail.com>
2021-12-09 12:43:41 +0545

    Fix key problem in spinner

--- src/components/Spinner/index.tsx
+++ src/components/Spinner/index.tsx
@@ -5,14 +5,14 @@ import { genericMemo } from '../../utils';
 import styles from './styles.css';
 
 const points = [
-    [0.5, 0],
-    [0.854, 0.146],
-    [1, 0.5],
-    [0.854, 0.854],
-    [0.5, 1],
-    [0.146, 0.854],
-    [0, 0.5],
-    [0.146, 0.146],
+    { left: 0.5, top: 0, key: 1 },
+    { left: 0.854, top: 0.146, key: 2 },
+    { left: 1, top: 0.5, key: 3 },
+    { left: 0.854, top: 0.854, key: 4 },
+    { left: 0.5, top: 1, key: 5 },
+    { left: 0.146, top: 0.854, key: 6 },
+    { left: 0, top: 0.5, key: 7 },
+    { left: 0.146, top: 0.146, key: 8 },
 ];
 
 export interface Props {
@@ -37,11 +37,11 @@ function Spinner(props: Props) {
             <div className={styles.pointsContainer}>
                 {points.map((point, i) => (
                     <div
-                        key={i}
+                        key={point.key}
                         className={styles.point}
                         style={{
-                            left: `${100 * point[0]}%`,
-                            top: `${100 * point[1]}%`,
+                            left: `${100 * point.left}%`,
+                            top: `${100 * point.top}%`,
                             animationDelay: `calc(${i} * var(--dui-duration-delay-short))`,
                             animationDuration: `calc(${points.length} * var(--dui-duration-delay-short))`,
                         }}
