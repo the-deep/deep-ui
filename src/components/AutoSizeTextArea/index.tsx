5e058fb2805ffc90bd11d6d5824d5e0dd3b74ffe
frozenhelium <fren.ankit@gmail.com>
2021-12-27 14:39:21 +0545

    Improve autosize textarea

--- src/components/AutoSizeTextArea/index.tsx
+++ src/components/AutoSizeTextArea/index.tsx
@@ -1,4 +1,4 @@
-import React, { useLayoutEffect, useRef, useState } from 'react';
+import React from 'react';
 import { _cs } from '@togglecorp/fujs';
 
 import styles from './styles.css';
@@ -6,61 +6,24 @@ import styles from './styles.css';
 interface AutoSizeTextAreaProps extends Omit<React.HTMLProps<HTMLTextAreaElement>, 'ref'> {
     elementRef?: React.RefObject<HTMLTextAreaElement>;
 }
+
 function AutoSizeTextArea(props: AutoSizeTextAreaProps) {
     const {
         className,
         elementRef,
         value,
-        onChange,
         ...otherProps
     } = props;
 
-    const [initialValue] = useState(value);
-
-    const textAreaContainerRef = useRef<HTMLDivElement>(null);
-
-    const handleInputChange = React.useCallback(
-        (e: React.FormEvent<HTMLTextAreaElement>) => {
-            const {
-                currentTarget: {
-                    value: v,
-                },
-            } = e;
-
-            const elem = textAreaContainerRef?.current;
-            if (elem) {
-                elem.dataset.replicatedValue = v;
-            }
-
-            if (onChange) {
-                onChange(e);
-            }
-        },
-        [onChange],
-    );
-
-    // NOTE: implemented from https://css-tricks.com/the-cleanest-trick-for-autogrowing-textareas/
-    useLayoutEffect(
-        () => {
-            const elem = textAreaContainerRef?.current;
-            if (elem) {
-                // FIXME: may need to call requestidlecallback
-                elem.dataset.replicatedValue = initialValue as string | undefined;
-            }
-        },
-        [initialValue],
-    );
-
     return (
-        <div
-            className={styles.rawTextAreaContainer}
-            ref={textAreaContainerRef}
-        >
+        <div className={_cs(styles.autoSizeTextArea, className)}>
+            <div className={styles.preview}>
+                {value}
+            </div>
             <textarea
-                className={_cs(styles.rawTextArea, className)}
+                className={styles.input}
                 ref={elementRef}
                 value={value}
-                onChange={handleInputChange}
                 {...otherProps}
             />
         </div>
