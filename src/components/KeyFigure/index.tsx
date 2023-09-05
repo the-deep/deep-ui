c989934ba56dff5a334a94e1ccd4c09dc554e28a
Aditya Khatri <adityakhatri47@gmail.com>
2023-09-05 09:43:53 +0545

    Add sizes and small boolean to key figure

--- src/components/KeyFigure/index.tsx
+++ src/components/KeyFigure/index.tsx
@@ -3,29 +3,59 @@ import { _cs } from '@togglecorp/fujs';
 
 import TextOutput from '../TextOutput';
 import { genericMemo } from '../../utils';
+import { SpacingTypes } from '../../types';
 
 import styles from './styles.css';
+
+const spacingToStyleMap: {
+    [key in SpacingTypes]: string;
+} = {
+    none: styles.noSpacing,
+    compact: styles.compactSpacing,
+    comfortable: styles.comfortableSpacing,
+    loose: styles.looseSpacing,
+};
 
 export interface Props {
     className?: string;
     label: string;
     value?: number;
+    valueClassName?: string;
+    descriptionClassName?: string;
+    spacing?: SpacingTypes;
+    small?: boolean;
 }
 
 function KeyFigure(props: Props) {
     const {
         className,
         label,
+        spacing = 'comfortable',
+        valueClassName,
+        descriptionClassName,
+        small,
         value,
     } = props;
 
     return (
         <TextOutput
-            className={_cs(styles.keyFigure, className)}
+            className={_cs(
+                styles.keyFigure,
+                className,
+                spacingToStyleMap[spacing],
+            )}
             value={value}
             valueType="number"
-            valueContainerClassName={styles.value}
-            descriptionContainerClassName={styles.description}
+            valueContainerClassName={_cs(
+                styles.value,
+                small && styles.small,
+                valueClassName,
+            )}
+            descriptionContainerClassName={_cs(
+                styles.description,
+                small && styles.small,
+                descriptionClassName,
+            )}
             description={label}
         />
     );
